6d9604b3fd728a8f737762ada63aafa465118cf2
Robert Wayne <robertjwayne@gmail.com>
2017-06-07 19:48:32 -0400

    Fix decorate function to properly call apply

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-const cake = {
-  name: 'German Chocolate Cake',
-  ingredients: ['eggs', 'flour', 'oil', 'chocolate', 'sugar', 'butter'],
-  topping: 'coconut frosting',
-  bakeTemp: '425 degrees',
-  bakeTime: '45 minutes',
-  customer: 'Tommy',
-  decorate(updateFunction) {
-    const status = `Decorating with ${this.topping}. Ready to eat soon!`;
-    updateFunction(status);
-    setTimeout(function () {
-      updateFunction(serve.apply(this, 'Happy Eating!', this.customer));
-    }, 2000);
-  },
-};
-
-const pie = {
-  name: 'Apple Pie',
-  ingredients: ['apples', 'flour', 'eggs', 'butter', 'sugar'],
-  topping: 'streusel',
-  bakeTemp: '350 degrees',
-  bakeTime: '75 minutes',
-  customer: 'Tammy',
-};
-
 function makeCake() {
   let updateCakeStatus;
   mix(updateCakeStatus);
@@ -68,6 +43,31 @@ function serve(message, customer) {
   // you shouldn't need to alter this function
   return (`${customer}, your ${this.name} is ready to eat! ${message}`);
 }
+
+const cake = {
+  name: 'German Chocolate Cake',
+  ingredients: ['eggs', 'flour', 'oil', 'chocolate', 'sugar', 'butter'],
+  topping: 'coconut frosting',
+  bakeTemp: '425 degrees',
+  bakeTime: '45 minutes',
+  customer: 'Tommy',
+  decorate(updateFunction) {
+    const status = `Decorating with ${this.topping}. Ready to eat soon!`;
+    updateFunction(status);
+    setTimeout(() => {
+      updateFunction(serve.apply(this, ['Happy Eating!', this.customer]));
+    }, 2000);
+  },
+};
+
+const pie = {
+  name: 'Apple Pie',
+  ingredients: ['apples', 'flour', 'eggs', 'butter', 'sugar'],
+  topping: 'streusel',
+  bakeTemp: '350 degrees',
+  bakeTime: '75 minutes',
+  customer: 'Tammy',
+};
 
 document.addEventListener('DOMContentLoaded', (event) => {
   // you shouldn't need to alter this function
